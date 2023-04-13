9af241c20c81dc5297508755845bdac5511facbc
robert <kaukana0@tuta.io>
2023-04-13 09:20:08 +0200

    intial commit

---
+++ markUpCode.mjs
@@ -0,0 +1,77 @@
+/*
+all HTML and CSS as JS string
+*/
+
+export default class MarkUpCode {
+
+	static mainElement() {
+		return `
+		<div id='menuContainer' tabindex="0">	</div>
+		`
+	}
+
+	static css(ms, zIndex) {
+		return `<style>
+		</style>`
+	}
+
+  /*
+  <div id="menuContainer">
+    <ul>
+      <li>
+        <a>Category</a>
+
+        <ul>    submenu
+          <li>
+            <a>Item</a>
+            <a>Item2</a>
+          </li>
+        </ul>
+
+      </li>
+    </ul>
+  </div>
+  */
+  static getMenuHtmlElements(data, callback) {
+		const ul = document.createElement('ul')
+    console.log(data)
+    for (let [name, items] of data) {
+      const li = document.createElement('li')
+      const a = document.createElement('a')
+      a.innerText = name
+      li.appendChild(a)
+      const x = this.getSubMenu(items, callback)
+      if(x) {
+        li.appendChild(x)
+      }
+      li.addEventListener('click', (ev) => callback(name))
+      ul.appendChild(li)
+    }
+    return ul
+  }
+
+  static getSubMenu(items, callback) {
+    if(items.length===0) {
+      return
+    } else {
+      const ul = document.createElement('ul')
+      items.forEach((name) => {
+        const li = document.createElement('li')
+        const a = document.createElement('a')
+        a.innerText = name
+        li.appendChild(a)
+        li.addEventListener('click', (ev) => {ev.stopImmediatePropagation(); callback(name)})
+        ul.appendChild(li)
+      })
+      return ul
+  }
+}
+
+	// helper
+	static getHtmlTemplate(source) {
+		const t = document.createElement('template')
+		t.innerHTML = source
+		return t.content
+	}
+
+}
---
+++ menu.mjs
@@ -0,0 +1,67 @@
+import MarkUpCode from  "./markUpCode.mjs"		// keep this file html/css free
+
+
+class Element extends HTMLElement {
+
+  #_callback
+  #_isInitialized
+
+	#$(elementId) {
+		return this.shadowRoot.getElementById(elementId)
+	}
+
+	constructor() {
+		super()
+		this.attachShadow({ mode: 'open' })
+		const tmp = MarkUpCode.getHtmlTemplate(MarkUpCode.mainElement()).cloneNode(true)
+		this.shadowRoot.appendChild(tmp)
+	}
+
+	#registerEvents() {
+	}
+
+	connectedCallback() {
+		if(!this.#_isInitialized) {
+			this.#registerEvents()	
+			this.#_isInitialized = true
+		}
+	}
+
+	disconnectedCallback() {
+	}
+
+	set data(val) {
+		// todo: clear
+    this.#$("menuContainer").appendChild(MarkUpCode.getMenuHtmlElements(val, this.onClick))
+	}
+
+	set callback(val) {
+		this.#_callback = val
+	}
+
+  static get observedAttributes() {
+		return ['data', "callback"]
+	}
+
+	attributeChangedCallback(name, oldVal, newVal) {
+		if (name === 'data' || name === 'callback') {
+			console.warn("menu: setting "+name+" via html attribute is being ignored. please use js property instead.")
+		}
+	}
+
+  onClick(el) {
+    console.log(el)
+    //this.#invokeCallback()
+  }
+
+  #invokeCallback() {
+		if(this.#_callback !== undefined) {
+			this.#_callback()
+		} else {
+			console.debug("menu: No callback")
+		}
+	}
+
+}
+
+window.customElements.define('ecl-like-menu', Element)
